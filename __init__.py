c504334cdeea909e75069243966b271313df923d
Idan Regev <idanre1@gmail.com>
2023-05-28 17:26:24 +0300

    pgk

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from stats_math.correlation.src.corr_features import compute_features_from_correl
 from stats_math.kde.src.optuna_cv import estimate_kde
 from stats_math.kde.src.optuna_cv import get_linspace_from_kde
 from stats_math.kde.src.optuna_cv import plot_kde
