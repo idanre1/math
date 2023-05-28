4eda4265702cec4383fb0c8f0b66dd55ef3a8bf7
Idan Regev <idanre1@gmail.com>
2023-05-28 15:14:56 +0300

    new pkg

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from stats_math.kde.src.optuna_cv import get_linspace_from_kde
 from stats_math.kde.src.optuna_cv import plot_kde
 from stats_math.kde.src.optuna_cv import cdf_from_kde
 from stats_math.kde.src.optuna_cv import pval_from_cdf
-from stats_math.src.tests import white_test
+from stats_math.stats_tests.tests import white_test
